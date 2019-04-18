016c0fb6c94392eace272010f2158834acf6a892
MartinAlejandroDiaz <diazmartin_95@yahoo.com.ar>
2019-04-18 15:36:46 -0300

    creo el proyecto

---
+++ TP-MatematicaSuperior-1C2019/TP-MatematicaSuperior-1C2019/Form1.designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TP_MatematicaSuperior_1C2019
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(40, 254);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(249, 51);
+            this.button5.TabIndex = 9;
+            this.button5.Text = " Interfaz Gráfica";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(221, 197);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(249, 51);
+            this.button4.TabIndex = 8;
+            this.button4.Text = " Suma Fasores ";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(31, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(249, 51);
+            this.button3.TabIndex = 7;
+            this.button3.Text = " Operaciones Avanzadas";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(221, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(249, 51);
+            this.button2.TabIndex = 6;
+            this.button2.Text = " Operaciones Básicas ";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(249, 51);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Estructura Datos y Transformaciones";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 355);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
