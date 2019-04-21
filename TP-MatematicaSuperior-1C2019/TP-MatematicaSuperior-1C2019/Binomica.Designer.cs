ff1ec29ce788ea9cf8c638fbfb8fb54dc25855fa
MartinAlejandroDiaz <diazmartin_95@yahoo.com.ar>
2019-04-20 21:41:52 -0300

    1

--- TP-MatematicaSuperior-1C2019/TP-MatematicaSuperior-1C2019/Binomica.Designer.cs
+++ TP-MatematicaSuperior-1C2019/TP-MatematicaSuperior-1C2019/Binomica.Designer.cs
@@ -202,6 +202,7 @@
             this.button7.TabIndex = 15;
             this.button7.Text = "Polar Z1";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -211,6 +212,7 @@
             this.button8.TabIndex = 16;
             this.button8.Text = "Polar Z2";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // textBox1
             // 
--- TP-MatematicaSuperior-1C2019/TP-MatematicaSuperior-1C2019/Binomica.cs
+++ TP-MatematicaSuperior-1C2019/TP-MatematicaSuperior-1C2019/Binomica.cs
@@ -20,7 +20,14 @@ namespace TP_MatematicaSuperior_1C2019
             textBox3.Text = "1";
             textBox4.Text = "1";
         }
-
+        private double modulo(double a ,double b)
+        {
+            return Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+        }
+        private double argumento(double a, double b)
+        {
+            return Math.Atan(b/a) * 180 / Math.PI;
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             double z, x;
@@ -46,11 +53,30 @@ namespace TP_MatematicaSuperior_1C2019
         }
         private void button4_Click(object sender, EventArgs e)
         {
-            double z, x;
-            z = (Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox3.Text)) - (Convert.ToDouble(textBox2.Text) *(-1)* Convert.ToDouble(textBox4.Text)) / (Math.Pow(Convert.ToDouble(textBox3.Text),2)+Math.Pow(Convert.ToDouble(textBox4.Text),2));
-            x = (Convert.ToDouble(textBox1.Text) *(-1) * Convert.ToDouble(textBox4.Text)) + (Convert.ToDouble(textBox3.Text) * Convert.ToDouble(textBox2.Text)) / (Math.Pow(Convert.ToDouble(textBox3.Text), 2) + Math.Pow(Convert.ToDouble(textBox4.Text), 2));
+            double z, x, a, b, c, d;
+            a = Convert.ToDouble(textBox1.Text);
+            c = Convert.ToDouble(textBox3.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            d = Convert.ToDouble(textBox4.Text);
+            z = (a * c - (b *(-1)* d)) / (Math.Pow(c,2)+Math.Pow(d,2));
+            x = (a *(-1) * d) + (c * b) / (Math.Pow(c, 2) + Math.Pow(d, 2));
             label8.Text = "(" + Convert.ToString(z) + "," + Convert.ToString(x) + ")";
         }
 
+        private void button7_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            label8.Text = "[" + Convert.ToString(modulo(a,b)) + "," + Convert.ToString(argumento(a,b)) + "]";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            a = Convert.ToDouble(textBox3.Text);
+            b = Convert.ToDouble(textBox4.Text);
+            label8.Text = "[" + Convert.ToString(modulo(a, b)) + "," + Convert.ToString(argumento(a, b)) + "]";
+        }
     }
 }
